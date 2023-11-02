9cb31fc6ec0215f4b2aa904dcb4f6676657d6129
Luna664 <luna.nayah@gmx.fr>
2023-11-02 09:22:04 +0100

    Mis à jour pour la version TECH-PREVIEW

--- StarCitizen - Traduction FR.ps1
+++ StarCitizen - Traduction FR.ps1
@@ -1,10 +1,14 @@
 ﻿$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $localization1 = Join-Path $ScriptDir "LIVE"
 $localization2 = Join-Path $ScriptDir "PTU"
+$localization3 = Join-Path $ScriptDir "TECH-PREVIEW"
 
 function Crédit {
     Write-Host "Script créé par:
 -Luna664
+
+Contributeurs:
+[>ĈIRKAU<] ∞ Pan
 
 Sources des traductions:
 SPEEDOU: https://github.com/SPEED0U/StarCitizenFrenchTranslation
@@ -15,18 +19,20 @@ function Menu {
     Clear-Host
     Crédit
     Write-Host "Traduction de SPEEDOU:"
-    Write-Host "1. Traduire le Live"
-    Write-Host "2. Traduire le PTU"
-    Write-Host "3. Aidé à la traduction"
+    Write-Host "1. Traduire la version Live"
+    Write-Host "2. Traduire la version PTU"
+    Write-Host "3. Traduire la version Tech-Preview"
+    Write-Host "4. Aidé à la traduction"
     Write-Host
     Write-Host "Traduction du Cirque Lisoir"
-    Write-Host "4. Traduire le Live"
-    Write-Host "5. Traduire le PTU"
-    Write-Host "6. Aidé à la traduction"
+    Write-Host "5. Traduire la version Live"
+    Write-Host "6. Traduire la version PTU"
+    Write-Host "7. Traduire la version Tech-Preview"
+    Write-Host "8. Aidé à la traduction"
     Write-Host
     Write-Host "Menu:"
-    Write-Host "7. Créer un raccourci sur le bureau"
-    Write-Host "8. Quitter"
+    Write-Host "9. Créer un raccourci sur le bureau"
+    Write-Host "10. Quitter"
 }
 
 function Create-Shortcut {
@@ -48,7 +54,6 @@ while ($true) {
         '1' {
             Clear-Host
             Write-Host "Traduction de la version Live."
-
             Write-Host "Vérification de la présence du Live."
             if (-not (Test-Path -Path $localization1 -PathType Container)) {
             Write-Host "La version Live n'a pas été trouvé, Vérifier que le script est placé dans le dossier StarCitizen et que le dossier LIVE s'y trouve, puis recommencé. (Aucune modifications n'a eu lieu)"
@@ -97,7 +102,33 @@ g_languageAudio = english'
             Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
         }
     }
-        '3'{
+        '3' {
+            Clear-Host
+            Write-Host "Traduction de la version Tech-Preview."
+            Write-Host "Vérification de la présence de la Tech-Preview."
+            if (-not (Test-Path -Path $localization3 -PathType Container)) {
+                Write-Host "La version Tech-Preview n'a pas été trouvé, Vérifier que le script est placé dans le dossier StarCitizen et que le dossier TECH-PREVIEW s'y trouve, puis recommencé. (Aucune modifications n'a eu lieu)"
+                Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
+            }
+            else {
+            Write-Host "Création du fichier user.cfg dans la version Tech-Preview..."
+            Set-Content -Path "$localization3\user.cfg" -Value 'g_language = french_(france)
+g_languageAudio = english'
+            Write-Host "Création du fichier user.cfg effectuer."
+
+            Write-Host "Création des dossier nécessaire dans la version Tech-Preview..."
+            $techpreviewLocalizationDir = Join-Path $localization3 "data\Localization\french_(france)"
+            if (-not (Test-Path -Path $techpreviewLocalizationDir -PathType Container)) {
+                New-Item -Path $techpreviewLocalizationDir -ItemType Directory -Force
+            }
+            Write-Host "Création des dossier effectuer."
+            Write-Host "Téléchargement du fichier de traduction dans la version Tech-Preview..."
+            Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SPEED0U/StarCitizenFrenchTranslation/main/global.ini" -OutFile "$techpreviewLocalizationDir\global.ini"
+            Write-Host "Téléchargement du dernier fichier de traduction dans la version Tech-preview terminé, Vous pouvez maintenant quité et lancé votre jeux, il est recommandé de retéléchargé la traduction de temps en temps pour recevoir la dernière version. Amuse toi bien sur Pyro citoyen(ne) !"
+            Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
+        }
+    }
+        '4' {
             Clear-Host
             Write-Host "Merci de contribuer à la traduction, votre aide seras certainement d'une grande aide!"
             Write-Host "Ouverture de la page internet..."
@@ -105,7 +136,7 @@ g_languageAudio = english'
             Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
         }
         # Traduction Cirque Lisoir
-        '4' {
+        '5' {
             Clear-Host
             Write-Host "Traduction de la version Live."
             Write-Host "Vérification de la présence du Live."
@@ -130,7 +161,7 @@ g_languageAudio = english'
             Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
         }
     }
-        '5' {
+        '6' {
             Clear-Host
             Write-Host "Traduction de la version PTU."
 
@@ -157,7 +188,33 @@ g_languageAudio = english'
             Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
         }
     }
-        '6'{
+        '7'{
+Clear-Host
+            Write-Host "Traduction de la version Tech-Preview."
+            Write-Host "Vérification de la présence de la Tech-Preview."
+            if (-not (Test-Path -Path $localization3 -PathType Container)) {
+                Write-Host "La version Tech-Preview n'a pas été trouvé, Vérifier que le script est placé dans le dossier StarCitizen et que le dossier TECH-PREVIEW s'y trouve, puis recommencé. (Aucune modifications n'a eu lieu)"
+                Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
+            }
+            else {
+            Write-Host "Création du fichier user.cfg dans la version Tech-Preview..."
+            Set-Content -Path "$localization3\user.cfg" -Value 'g_language = french_(france)
+g_languageAudio = english'
+            Write-Host "Création du fichier user.cfg effectuer."
+
+            Write-Host "Création des dossier nécessaire dans la version Tech-Preview..."
+            $techpreviewLocalizationDir = Join-Path $localization3 "data\Localization\french_(france)"
+            if (-not (Test-Path -Path $techpreviewLocalizationDir -PathType Container)) {
+                New-Item -Path $techpreviewLocalizationDir -ItemType Directory -Force
+            }
+            Write-Host "Création des dossier effectuer."
+            Write-Host "Téléchargement du fichier de traduction dans la version Tech-Preview..."
+            Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SPEED0U/StarCitizenFrenchTranslation/main/global.ini" -OutFile "$techpreviewLocalizationDir\global.ini"
+            Write-Host "Téléchargement du dernier fichier de traduction dans la version Tech-preview terminé, Vous pouvez maintenant quité et lancé votre jeux, il est recommandé de retéléchargé la traduction de temps en temps pour recevoir la dernière version. Amuse toi bien sur Pyro citoyen(ne) !"
+            Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
+        }
+    }
+        '8'{
             Clear-Host
             Write-Host "Merci de contribuer à la traduction, votre aide seras certainement d'une grande aide!"
             Write-Host "Ouverture de la page internet..."
@@ -165,13 +222,13 @@ g_languageAudio = english'
             Read-Host "Appuyez sur Entrée pour revenir au menu principal..."
         }
     #Menu
-        '7'{
+        '9'{
             Clear-Host
             Write-Host "Création du raccourci sur le bureau : $shortcutFile"
             Create-Shortcut
             Read-Host "Appuyez sur Entrée pour revenir au menu principal"
         }
-        '8'{
+        '10'{
             Exit
         }
         default {
